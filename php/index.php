0d1bcccdb19c70d0a9308e2d8f17b237b9b3c24c
jotavital <80636554+jotavital@users.noreply.github.com>
2021-07-14 16:21:05 -0300

    mudanças de visual

--- php/index.php
+++ php/index.php
@@ -6,12 +6,38 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/main.css">
     <title>AutoTranslate</title>
 </head>
+
+<style>
+    body {
+        background-color: aliceblue;
+    }
+
+    p {
+        margin: 0;
+        padding: 0;
+    }
+
+    .p-red {
+        color: red;
+    }
+
+    .p-blue {
+        color: blue;
+    }
+
+    .p-green {
+        color: green;
+    }
+</style>
+
 <body>
     <h1>Assistente de troca de palavras</h1>
 
@@ -21,11 +47,14 @@ session_start();
         <button type="submit">Enviar</button>
     </form>
 
-    <?php
-        if(isset($_SESSION['msg'])){
+    <div style="margin-top: 10px;">
+        <?php
+        if (isset($_SESSION['msg'])) {
             echo $_SESSION['msg'];
             unset($_SESSION['msg']);
         }
-    ?>
+        ?>
+    </div>
 </body>
+
 </html>
--- php/processaArquivo.php
+++ php/processaArquivo.php
@@ -12,17 +12,17 @@ $nomeArquivo = explode(".php", $_POST['nomeArquivo']);
 //verifica permissoes da pasta
 
 if(is_writable($dir)){
-    $_SESSION['msg'] .= "<br><p style='yellow'>O diretório lang tem as permissões necessárias!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-green'>O diretório lang tem as permissões necessárias!</p>";
 }else{
-    $_SESSION['msg'] .= "<br><p style='red'>O diretório lang NÃO tem as permissões necessárias!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-red'>O diretório lang NÃO tem as permissões necessárias!</p>";
 }
 
 //cria arquivo ini
 
 if (file_put_contents($dir . "pt-br_" . $nomeArquivo[0] . ".ini", FILE_APPEND) != false) {
-    $_SESSION['msg'] .= "<br><p style='green'>Arquivo .ini criado com sucesso!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-green'>Arquivo .ini criado com sucesso!</p>";
 } else {
-    $_SESSION['msg'] .= "<br><p style='red'>Erro ao criar arquivo .ini!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao criar arquivo .ini!</p>";
 }
 
 //substitui as palavras pelas label no arquivo destino
@@ -30,23 +30,23 @@ if (file_put_contents($dir . "pt-br_" . $nomeArquivo[0] . ".ini", FILE_APPEND) !
 $caminhoArquivo =  __DIR__ . "/../../applications/system/admin/" . $nomeArquivoComExtensao;
 
 if(($textoDoArquivo = file_get_contents($caminhoArquivo, FILE_TEXT)) != false){
-    $_SESSION['msg'] .= "<br><p style='yellow'>Consegui ler o arquivo " . $nomeArquivoComExtensao . "</p>";
+    $_SESSION['msg'] .= "<br><p class='p-blue'>Consegui ler o arquivo " . $nomeArquivoComExtensao . "</p>";
 }else{
-    $_SESSION['msg'] .= "<br><p style='red'>Erro ao ler o arquivo " . $nomeArquivoComExtensao . "</p>";
+    $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao ler o arquivo " . $nomeArquivoComExtensao . "</p>";
 }
 
 if(($oldStringsJson = file_get_contents("../baseStrings/oldStringsBase.json", FILE_TEXT)) != false){
-    $_SESSION['msg'] .= "<br><p style='yellow'>Consegui ler o arquivo oldStringsBase.json!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-blue'>Consegui ler o arquivo oldStringsBase.json!</p>";
     $oldStringsDecoded = json_decode($oldStringsJson, FALSE);
 }else{
-    $_SESSION['msg'] .= "<br><p style='red'>Erro ao ler o arquivo oldStringsBase.json!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao ler o arquivo oldStringsBase.json!</p>";
 }
 
 if(($newStringsJson = file_get_contents("../baseStrings/newStringsBase.json", FILE_TEXT)) != false){
-    $_SESSION['msg'] .= "<br><p style='yellow'>Consegui ler o arquivo newStringsBase.json!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-blue'>Consegui ler o arquivo newStringsBase.json!</p>";
     $newStringsDecoded = json_decode($newStringsJson, FALSE);
 }else{
-    $_SESSION['msg'] .= "<br><p style='red'>Erro ao ler o arquivo newStringsBase.json!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao ler o arquivo newStringsBase.json!</p>";
 }
 
 //substitui as simpleLabels
@@ -58,9 +58,9 @@ foreach ($oldStringsDecoded->simpleLabel as $nomePropriedadeOld => $palavraOld) 
 
         if ($nomePropriedadeNew == $nomePropriedadeOld) {
             if(($textoDoArquivo = str_replace($palavraOld, $palavraNew, $textoDoArquivo)) != null){
-                $_SESSION['msg'] .= "<br><p style='yellow'>Consegui substituir a expressão " . $palavraOld . "</p>";
+                $_SESSION['msg'] .= "<br><p class='p-blue'>Consegui substituir a expressão " . $palavraOld . "</p>";
             }else{
-                $_SESSION['msg'] .= "<br><p style='red'>Erro ao substituir a expressão " . $palavraOld . "</p>";
+                $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao substituir a expressão " . $palavraOld . "</p>";
             }
         }
     }
@@ -76,9 +76,9 @@ foreach ($oldStringsDecoded->doubleQuoted->inHTML as $nomePropriedadeOld => $pal
 
         if ($nomePropriedadeNew == $nomePropriedadeOld) {
             if(($textoDoArquivo = str_replace($palavraOld, $palavraNew, $textoDoArquivo)) != null){
-                $_SESSION['msg'] .= "<br><p style='yellow'>Consegui substituir a expressão " . $palavraOld . "</p>";
+                $_SESSION['msg'] .= "<br><p class='p-blue'>Consegui substituir a expressão " . $palavraOld . "</p>";
             }else{
-                $_SESSION['msg'] .= "<br><p style='red'>Erro ao substituir a expressão " . $palavraOld . "</p>";
+                $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao substituir a expressão " . $palavraOld . "</p>";
             }
         }
     }
@@ -94,9 +94,9 @@ foreach ($oldStringsDecoded->doubleQuoted->inPHP as $nomePropriedadeOld => $pala
 
         if ($nomePropriedadeNew == $nomePropriedadeOld) {
             if(($textoDoArquivo = str_replace($palavraOld, $palavraNew, $textoDoArquivo)) != null){
-                $_SESSION['msg'] .= "<br><p style='yellow'>Consegui substituir a expressão " . $palavraOld . "</p>";
+                $_SESSION['msg'] .= "<br><p class='p-blue'>Consegui substituir a expressão " . $palavraOld . "</p>";
             }else{
-                $_SESSION['msg'] .= "<br><p style='red'>Erro ao substituir a expressão " . $palavraOld . "</p>";
+                $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao substituir a expressão " . $palavraOld . "</p>";
             }
         }
     }
@@ -105,15 +105,15 @@ foreach ($oldStringsDecoded->doubleQuoted->inPHP as $nomePropriedadeOld => $pala
 $pastaAdmin = __DIR__ . "/../../applications/system/admin/";
 
 if(is_writable($pastaAdmin)){
-    $_SESSION['msg'] .= "<br><p style='yellow'>O diretório admin tem as permissões necessárias!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-green'>O diretório admin tem as permissões necessárias!</p>";
 }else{
-    $_SESSION['msg'] .= "<br><p style='red'>O diretório admin NÃO tem as permissões necessárias!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-red'>O diretório admin NÃO tem as permissões necessárias!</p>";
 }
 
 if(file_put_contents($caminhoArquivo, $textoDoArquivo) != false){
-    $_SESSION['msg'] .= "<br><p style='green'>Arquivo " . $nomeArquivoComExtensao . " finalizado com sucesso!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-green'>Arquivo " . $nomeArquivoComExtensao . " finalizado com sucesso!</p>";
 }else{
-    $_SESSION['msg'] .= "<br><p style='red'>Erro ao escrever no arquivo " . $nomeArquivoComExtensao . "!</p>";
+    $_SESSION['msg'] .= "<br><p class='p-red'>Erro ao escrever no arquivo " . $nomeArquivoComExtensao . "!</p>";
 }
 
 header("Location: index.php");
