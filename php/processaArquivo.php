d1c50df9535c62924d956c06129818492af5aacb
jotavital <80636554+jotavital@users.noreply.github.com>
2021-07-14 12:18:43 -0300

    alterando expressões com sucesso

--- php/processaArquivo.php
+++ php/processaArquivo.php
@@ -1,19 +1,99 @@
 <?php
 
-if(!isset($_SESSION)){
+if (!isset($_SESSION)) {
     session_start();
 }
 
 $dir = (__DIR__ . "/../../applications/system/lang/portugues-brasil/");
 
+$nomeArquivoComExtensao = $_POST['nomeArquivo'];
 $nomeArquivo = explode(".php", $_POST['nomeArquivo']);
 
-if(file_put_contents( $dir . "pt-br_" . $nomeArquivo[0] . ".ini", FILE_APPEND) != false){
+//cria arquivo ini
+
+if (file_put_contents($dir . "pt-br_" . $nomeArquivo[0] . ".ini", FILE_APPEND) != false) {
     $_SESSION['msg'] = "Arquivo .ini criado com sucesso!";
-}else{
+} else {
     $_SESSION['msg'] = "Erro ao criar arquivo .ini!";
 }
 
-header("Location: index.php");
+//substitui as palavras pelas label no arquivo destino
 
-?>
+$caminhoArquivo =  __DIR__ . "/../../applications/system/admin/" . $nomeArquivoComExtensao;
+
+if(($textoDoArquivo = file_get_contents($caminhoArquivo, FILE_TEXT)) != false){
+    $_SESSION['msg'] .= "<br>Consegui ler o arquivo " . $nomeArquivoComExtensao;
+}else{
+    $_SESSION['msg'] .= "<br>Erro ao ler o arquivo " . $nomeArquivoComExtensao;
+}
+
+if(($oldStringsJson = file_get_contents("../baseStrings/oldStringsBase.json", FILE_TEXT)) != false){
+    $_SESSION['msg'] .= "<br>Consegui ler o arquivo oldStringsBase.json";
+    $oldStringsDecoded = json_decode($oldStringsJson, FALSE);
+}else{
+    $_SESSION['msg'] .= "<br>Erro ao ler o arquivo oldStringsBase.json";
+}
+
+if(($newStringsJson = file_get_contents("../baseStrings/newStringsBase.json", FILE_TEXT)) != false){
+    $_SESSION['msg'] .= "<br>Consegui ler o arquivo newStringsBase.json";
+    $newStringsDecoded = json_decode($newStringsJson, FALSE);
+}else{
+    $_SESSION['msg'] .= "<br>Erro ao ler o arquivo newStringsBase.json";
+}
+
+//substitui as simpleLabels
+foreach ($oldStringsDecoded->simpleLabel as $nomePropriedadeOld => $palavraOld) {
+    $nomePropriedadeOld = explode("old_", $nomePropriedadeOld);
+
+    foreach ($newStringsDecoded->simpleLabel as $nomePropriedadeNew => $palavraNew) {
+        $nomePropriedadeNew = explode("new_", $nomePropriedadeNew);
+
+        if ($nomePropriedadeNew == $nomePropriedadeOld) {
+            if(($textoDoArquivo = str_replace($palavraOld, $palavraNew, $textoDoArquivo)) != null){
+                $_SESSION['msg'] .= "<br>Consegui substituir a expressão " . $palavraOld;
+            }else{
+                $_SESSION['msg'] .= "<br>Erro ao substituir a expressão " . $palavraOld;
+            }
+        }
+    }
+}
+
+//substitui as doubleQuoted inHTML
+
+foreach ($oldStringsDecoded->doubleQuoted->inHTML as $nomePropriedadeOld => $palavraOld) {
+    $nomePropriedadeOld = explode("old_", $nomePropriedadeOld);
+
+    foreach ($newStringsDecoded->doubleQuoted->inHTML as $nomePropriedadeNew => $palavraNew) {
+        $nomePropriedadeNew = explode("new_", $nomePropriedadeNew);
+
+        if ($nomePropriedadeNew == $nomePropriedadeOld) {
+            if(($textoDoArquivo = str_replace($palavraOld, $palavraNew, $textoDoArquivo)) != null){
+                $_SESSION['msg'] .= "<br>Consegui substituir a expressão " . $palavraOld;
+            }else{
+                $_SESSION['msg'] .= "<br>Erro ao substituir a expressão " . $palavraOld;
+            }
+        }
+    }
+}
+
+//substitui as doubleQuoted inPHP
+
+foreach ($oldStringsDecoded->doubleQuoted->inPHP as $nomePropriedadeOld => $palavraOld) {
+    $nomePropriedadeOld = explode("old_", $nomePropriedadeOld);
+
+    foreach ($newStringsDecoded->doubleQuoted->inPHP as $nomePropriedadeNew => $palavraNew) {
+        $nomePropriedadeNew = explode("new_", $nomePropriedadeNew);
+
+        if ($nomePropriedadeNew == $nomePropriedadeOld) {
+            if(($textoDoArquivo = str_replace($palavraOld, $palavraNew, $textoDoArquivo)) != null){
+                $_SESSION['msg'] .= "<br>Consegui substituir a expressão " . $palavraOld;
+            }else{
+                $_SESSION['msg'] .= "<br>Erro ao substituir a expressão " . $palavraOld;
+            }
+        }
+    }
+}
+
+file_put_contents($caminhoArquivo, $textoDoArquivo);
+
+header("Location: index.php");
